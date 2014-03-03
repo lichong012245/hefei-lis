003d1cf5f740050bd32ae66e0809b4ab5cf794bb
root <root@lubuntu.(none)>
2014-03-03 17:47:15 -0500

    hook up auth using django built-in views and forms

--- lis/urls.py
+++ lis/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.conf import settings
 from dajaxice.core import dajaxice_autodiscover
-from django.contrib import admin
+from django.contrib import admin, auth
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
 
 dajaxice_autodiscover()
 admin.autodiscover()
@@ -10,6 +12,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     
     url(r'^family/', include('family.urls')),
+    url(r'^register/', CreateView.as_view(template_name='registration/register.html',form_class=UserCreationForm,success_url='/')),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+  
     
 
     (r'^%s/' % settings.DAJAXICE_MEDIA_PREFIX, include('dajaxice.urls')),
