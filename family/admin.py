d0cba4c8df31b7c07cedd6acba2ec1f26c6659b1
root <root@lubuntu.(none)>
2014-03-04 18:16:38 -0500

    add userprofile model; widget to textarea

--- family/admin.py
+++ family/admin.py
@@ -1,19 +1,27 @@
 from django.contrib import admin
 from treebeard.admin import TreeAdmin
 from treebeard.forms import movenodeform_factory
-from family.models import Member, postimage
+from family.models import Member, postimage, UserProfile
+from django import forms
 
 
 class ImageInline(admin.TabularInline):
     model = postimage
 
+class UserProfileForm(forms.ModelForm):
+    desc = forms.CharField(widget=forms.Textarea)
+    class Meta:
+            model = UserProfile
 
 class MyAdmin(TreeAdmin):
     form = movenodeform_factory(Member)
     inlines = [
         ImageInline,
     ]
-
+    
+class UserProfileAdmin(admin.ModelAdmin):
+    form = UserProfileForm
 
 
 admin.site.register(Member, MyAdmin)
+admin.site.register(UserProfile,UserProfileAdmin)
--- family/models.py
+++ family/models.py
@@ -4,6 +4,8 @@ from treebeard.mp_tree import MP_Node
 from PIL import Image
 from django.core.files.uploadedfile import SimpleUploadedFile
 import io
+from django.contrib.auth.models import User
+
 
 class Member(MP_Node):
    male_or_female=(
@@ -27,6 +29,21 @@ class Member(MP_Node):
    node_order_by = ['name']
    def __unicode__(self):
         return self.name
+
+class UserProfile(models.Model):
+  tree_branch=(
+('1',u'一房'),
+('2',u'二房'),
+('3',u'三房'),
+('4',u'四房'),
+('5',u'五房'),
+('5',u'六房'),
+)
+  user=models.OneToOneField(User)
+  member=models.OneToOneField(Member,blank=True)
+  self_desc=models.CharField(max_length=5000,blank=True)
+  branch=models.CharField(choices=tree_branch,max_length=10,blank=True)
+  
  
 class postimage(models.Model):
     image=models.ImageField(upload_to='family/orig/',blank=True, null=True)
