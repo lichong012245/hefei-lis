e2f0e2c9bb69876ca7d3667f72a9e0c4cae0fdca
root <root@lubuntu.(none)>
2014-03-11 17:24:36 -0400

    add linebreaksbr to ajax.py

--- family/ajax.py
+++ family/ajax.py
@@ -1,6 +1,8 @@
 from dajaxice.decorators import dajaxice_register
 from dajax.core import Dajax
 from family.models import Member
+from django.template.defaultfilters import linebreaksbr
+
 
 @dajaxice_register
 def get_member(req, pk):
@@ -8,6 +10,6 @@ def get_member(req, pk):
     p = Member.objects.get(pk=pk)
     dajax.assign('#name', 'innerHTML', p.name)
     dajax.assign('#sex', 'innerHTML', p.sex)    
-    dajax.assign('#desc', 'innerHTML', p.desc)    
+    dajax.assign('#desc', 'innerHTML', linebreaksbr(p.desc))    
     dajax.assign('#branch', 'innerHTML', p.branch)    
     return dajax.json()
