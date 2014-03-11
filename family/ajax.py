175692bf83f6c0879b1d4f1bacf5db42fe42bb3b
root <root@lubuntu.(none)>
2014-03-11 17:40:43 -0400

    add lambda to render sex with chinese unicode

--- family/ajax.py
+++ family/ajax.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from dajaxice.decorators import dajaxice_register
 from dajax.core import Dajax
 from family.models import Member
@@ -9,7 +10,7 @@ def get_member(req, pk):
     dajax = Dajax()
     p = Member.objects.get(pk=pk)
     dajax.assign('#name', 'innerHTML', p.name)
-    dajax.assign('#sex', 'innerHTML', p.sex)    
+    dajax.assign('#sex', 'innerHTML', lambda p.sex: u'男' if p.sex = 'M' else u'女')    
     dajax.assign('#desc', 'innerHTML', linebreaksbr(p.desc))    
     dajax.assign('#branch', 'innerHTML', p.branch)    
     return dajax.json()
