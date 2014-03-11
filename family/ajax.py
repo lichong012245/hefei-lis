437f0852c3469690d04682857829e18bf4bc1c71
root <root@lubuntu.(none)>
2014-03-11 18:15:52 -0400

    lambda fix

--- family/ajax.py
+++ family/ajax.py
@@ -10,7 +10,7 @@ def get_member(req, pk):
     dajax = Dajax()
     p = Member.objects.get(pk=pk)
     dajax.assign('#name', 'innerHTML', p.name)
-    dajax.assign('#sex', 'innerHTML', lambda p.sex: u'男' if p.sex = 'M' else u'女')    
+    dajax.assign('#sex', 'innerHTML', lambda p.sex: u'男' if p.sex =='M' else u'女')    
     dajax.assign('#desc', 'innerHTML', linebreaksbr(p.desc))    
     dajax.assign('#branch', 'innerHTML', p.branch)    
     return dajax.json()
