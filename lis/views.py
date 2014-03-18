6d6ce84693431827d7e72879b613be0635bbda46
user <user@lubuntu.(none)>
2014-03-17 20:19:20 -0400

    contact page

--- lis/views.py
+++ lis/views.py
@@ -4,6 +4,15 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django import forms
+from django.core.mail import send_mail
+
+class ContactForm(forms.Form):
+    sender = forms.CharField(max_length=100,widget=forms.TextInput(attrs={'id':'sender'}))
+    subject = forms.CharField(max_length=100,widget=forms.TextInput(attrs={'id':'sbuject'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'id':'email'}))
+    message=forms.CharField(widget=forms.Textarea(attrs={'id':'message'}))
 
 def Index(request):
     return render(request,'lis/index.html')
@@ -13,7 +22,18 @@ def Gallery(request):
 
 
 def Contact(request):
-      return render(request, 'lis/contact.html')
+      if request.method == 'POST':
+         form = ContactForm(request.POST)
+         if form.is_valid():
+              subject = form.cleaned_data['subject']
+              sender = form.cleaned_data['sender']
+              message = form.cleaned_data['message']
+              email = form.cleaned_data['email']
+              send_email(subject,message,sender,['lishefei@gmail.com'])
+              return HttpResponseRedirect('/')  
+      else:
+              form = ContactForm()
+      return render(request, 'lis/contact.html',{'form':form})
 
 def UserProfile(request):      
       if request.method == 'POST':           
