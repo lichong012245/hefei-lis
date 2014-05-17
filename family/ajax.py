c2a083798a1564673c294a31e4b8bd35195cd948
John Li <inventu.tyrone.lee@gmail.com>
2014-05-17 16:18:26 -0700

    ajax.py: change all() to order_by ()

--- family/ajax.py
+++ family/ajax.py
@@ -11,13 +11,15 @@ def show_sex_in_chinese(sex):
              return u'女'
 
 def get_first_thumbnail_of_member(object):
-           r=list(object.postimage_set.all()[:1])
+         try:
+           r=list(object.postimage_set.order_by('id')[0])
            if r:
                 return r[0].thumbnail.url
+         except IndexError:       
            return 'https://lishefei.s3.amazonaws.com/lis/images/lhz/placeholder.png'
 
 def get_nth_item_image_of_member(object,n):
-  r=list(object.postimage_set.all()[n-1:n])
+  r=list(object.postimage_set.order_by('id')[n-1:n])
   if r:
     return r[0].image.url
   return ''  
